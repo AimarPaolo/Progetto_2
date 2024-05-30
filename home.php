6d08ac7f7a3341770dd2e10faa97f17226c231ec
Paolo Aimar <s297424@studenti.polito.it>
2024-05-30 23:25:46 +0200

    Inserimento di immagini e video dentro le apposite cartelle + inizializzazione della home page --> head.

---
+++ home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <title>Home page artesina</title>
+        <meta name="author" content="Paolo Aimar">
+        <meta name="keywords" lang="it" content="html">
+        <meta name="description" content="Pagina di acquisto dei biglietti">
+        <!--in questo caso faccio un refresh della pagina ogni minuto per aggiornarla, le operazioni dell'utente non sono molte quindi ha 
+        senso aggiornarla ogni minuto-->
+        <meta http-equiv="refresh" content="60">
+        <link rel="stylesheet" href="Home_ESP2.css">
+        <!--per il momento inserisco il riferimento della pagina ma non so ancora se sia necessario utilizzare il codice js per controllare i dati-->
+        <script src="home_ESP2.js"></script>
+    </head>
+    <body>
+    https://www.w3schools.com/howto/howto_css_topnav_right.asp 
+    <!--prendere spunto domani da questo sito per controllare ed inserire le pagine e creare il menù. La grafica sembra molto carina,
+    ottimizzarlo magari cambiando i colori e le size.-->
+    </body>
